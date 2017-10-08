0c84fac32458e24fd21b643042a4934a15bb4c7c
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-10-08 14:59:56 +0300

    complete one to ten from 99

--- exercises/Ninety_Nine_Problems/A_One_to_Ten.hs
+++ exercises/Ninety_Nine_Problems/A_One_to_Ten.hs
@@ -1,5 +1,8 @@
 module A_One_to_Ten where
 
+import Data.List
+import Control.Arrow
+
 myLast :: [a] -> a
 myLast [] = error "No last in empty list"
 myLast [x] = x
@@ -20,3 +23,20 @@ myReverse = foldl (flip (:)) []
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs = xs == reverse xs
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List x) = concatMap flatten x
+
+compress :: (Eq a) => [a] -> [a]
+compress = map head . group
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = let (frst,rest) = span (==x) xs
+              in (x:frst) : pack rest
+
+encode :: (Eq a) => [a] -> [(Int,a)]
+encode = map (length &&& head) . pack
