c80ccf2540ce237a7e4e8dc33ab469ff1926b784
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-10-22 20:06:45 +0300

    BuildTower

---
+++ kata/AC_BuildTower.hs
@@ -0,0 +1,9 @@
+module AC_BuildTower where
+
+buildFloor :: Int -> Int -> String
+buildFloor i n = let spaces = replicate (n - i) ' '
+                     asterisks = replicate (2*i - 1) '*'
+                 in spaces ++ asterisks ++ spaces
+
+buildTower :: Int -> [String]
+buildTower n = map (`buildFloor` n) [1..n]
