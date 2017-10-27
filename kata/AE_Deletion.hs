37fd30143875ee0d2f8f5e3ffeacc3d82b8fe251
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-10-27 15:41:24 +0300

    Delete occurrences of an element if it occurs more than n times

---
+++ kata/AE_Deletion.hs
@@ -0,0 +1,15 @@
+module AE_Deletion where
+
+import qualified Data.Map as Map
+
+deleteNth :: [Int] -> Int -> [Int]
+deleteNth lst n =
+  reverse
+  . snd
+  . foldl (\acc x -> let updatedFreq = Map.insertWith (+) x 1 (fst acc)
+                         (Just freq) = Map.lookup x updatedFreq
+                         deletedLst = snd acc
+                         updatedLst =  if freq > n then deletedLst else x:deletedLst
+                     in (updatedFreq, updatedLst))
+          (Map.empty, [])
+  $ lst
