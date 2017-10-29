6e01036a75f5c985f17bcc3dd115076959eed8ae
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-10-29 03:26:38 +0300

    Checking Groups

---
+++ kata/AG_Groups.hs
@@ -0,0 +1,9 @@
+module AG_Groups where
+
+groupCheck :: String -> Bool
+groupCheck "" = True
+groupCheck s = let symbols = [('(',')'),('[',']'),('{','}')]
+                   (open, close) = span (`notElem` map snd symbols) s
+               in (null close == null open)
+                  && (last open, head close) `elem` symbols
+                  && groupCheck (init open ++ tail close)
