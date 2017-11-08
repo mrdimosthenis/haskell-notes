61aa60784976e4cfbac31394fba58c452ffc83b0
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-11-09 00:30:31 +0200

    Pascal's Triangle

---
+++ AO_PascalsTriangle.hs
@@ -0,0 +1,7 @@
+module AO_PascalsTriangle where
+
+pascalsTriangle :: Integer -> [Integer]
+pascalsTriangle x = [binCoefficient i j | i <- [0..x-1], j <- [0..i]]
+  where binCoefficient n k = factorial n `div` (factorial k * factorial (n - k))
+        factorial 0 = 1
+        factorial n = n * factorial (n - 1)
