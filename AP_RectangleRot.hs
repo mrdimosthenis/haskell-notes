1a3b77668ce750b240d9e5dfb91ecd199c9f2e94
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-11-16 22:18:27 +0200

    Happy^^ numbers

---
+++ AP_RectangleRot.hs
@@ -0,0 +1,19 @@
+module AP_RectangleRot where
+
+rotate :: (Float, Float) -> (Float, Float)
+rotate (x, y) = (x * cos v - y * sin v, x * sin v + y * cos v)
+  where v = negate pi / 4
+
+isInner :: (Float, Float) -> (Float, Float) -> Bool
+isInner (x, y) (w, h)
+  | abs rotX <= w && abs rotY <= h = True
+  | otherwise = False
+  where (rotX, rotY) = rotate (x, y)
+
+rectangleRot :: Int -> Int -> Int
+rectangleRot a b = let (w, h) = (fromIntegral a / 2, fromIntegral b / 2)
+                       r = max w h * 2
+                       range = [(-r)..r]
+                   in length
+                    . filter (`isInner` (w, h))
+                    $ [(x, y) | x <- range, y <- range]
---
+++ AR_IsHappy.hs
@@ -0,0 +1,15 @@
+module AR_IsHappy where
+
+import Data.Char (digitToInt)
+
+sumOfSquares :: Integer -> Integer
+sumOfSquares = toInteger . sum . map (product . replicate 2 . digitToInt) . show
+
+end :: [Integer] -> Integer
+end (x:xs)
+  | x `elem` xs = x
+  | otherwise = end (sumOfSquares x : x : xs)
+end _ = error "empty list"
+
+isHappy :: Integer -> Bool
+isHappy x = end [x] == 1
