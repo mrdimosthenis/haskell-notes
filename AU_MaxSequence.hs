083d63ae11f0d10edabf99210b4cbbfcac4d234c
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-12-03 23:24:18 +0200

    Maximum subarray sum

---
+++ AU_MaxSequence.hs
@@ -0,0 +1,8 @@
+module AU_MaxSequence where
+
+import Data.List (tails,inits)
+
+maxSequence :: [Int] -> Int
+maxSequence xs
+  | null xs || all (<0) xs = 0
+  | otherwise = maximum . concatMap (map sum . tail . inits) . tails $ xs
