934e296815d1bf7082ff53c1190a52ddb6806366
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-03-12 20:55:05 +0200

    change Rot13

--- AW_Rot13.hs
+++ AW_Rot13.hs
@@ -12,8 +12,5 @@ shift :: Char -> Char
 shift c = chr . (bs +) . (`mod` 26) . (13 +) . ((-bs) +) . ord $ c
   where bs = base c
 
-isShiftable :: Char -> Bool
-isShiftable c = (ord c >= 65 && ord c <= 90) || (ord c >= 97 && ord c <= 122)
-
 rot13 :: String -> String
-rot13 = map (\c -> if isShiftable c then shift c else c)
+rot13 = map (\c -> if isLetter c && isAscii c then shift c else c)
